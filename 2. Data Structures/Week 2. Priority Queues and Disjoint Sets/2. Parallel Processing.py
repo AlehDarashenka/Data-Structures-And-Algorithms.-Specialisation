6c9d67b0c4372dc6e984d63b515e45164d3a01cf
AlehDarashenka <aleh.darashenka@gmail.com>
2016-10-20 12:11:57 +0300

    script was updated

--- 2. Data Structures/Week 2. Priority Queues and Disjoint Sets/2. Parallel Processing.py
+++ 2. Data Structures/Week 2. Priority Queues and Disjoint Sets/2. Parallel Processing.py
@@ -1,3 +1,13 @@
+'''
+You have a program which is parallelized and uses n independent threads to process the given list
+of m jobs. Threads take jobs in the order they are given in the input. If there is a free thread,
+it immediately takes the next job from the list. If a thread has started processing a job, it doesn’t
+interrupt or stop until it finishes processing the job. If several threads try to take jobs from the list
+simultaneously, the thread with smaller index takes the job. For each job you know exactly how long
+will it take any thread to process this job, and this time is the same for all the threads. You need to
+determine for each job which thread will process it and when will it start processing.
+'''
+
 import Priority_Queue as pq
 
 class Priority_Queue_mod(pq.priority_queue):
@@ -8,18 +18,19 @@ class Priority_Queue_mod(pq.priority_queue):
     def sift_down(self, i):
         maxindex = i
         self.left_child(i)
-
-        if self.index < self.size and self.heap[self.index][1] < self.heap[maxindex][1]:
-            maxindex = self.index
-        elif self.heap[self.index][1] == self.heap[maxindex][1] and self.heap[self.index][0] < self.heap[maxindex][0]:
-            maxindex = self.index
+        if self.index <self.size:
+            if self.index < self.size and self.heap[self.index][1] < self.heap[maxindex][1]:
+                maxindex = self.index
+            elif self.heap[self.index][1] == self.heap[maxindex][1] and self.heap[self.index][0] < self.heap[maxindex][0]:
+                maxindex = self.index
 
         self.right_child(i)
 
-        if self.index < self.size and self.heap[self.index][1] < self.heap[maxindex][1]:
-            maxindex = self.index
-        elif self.heap[self.index][1] == self.heap[maxindex][1] and self.heap[self.index][0] < self.heap[maxindex][0]:
-            maxindex = self.index
+        if self.index <self.size:
+            if self.index < self.size and self.heap[self.index][1] < self.heap[maxindex][1]:
+                maxindex = self.index
+            elif self.heap[self.index][1] == self.heap[maxindex][1] and self.heap[self.index][0] < self.heap[maxindex][0]:
+                maxindex = self.index
 
         if maxindex != i:
             self.heap[i], self.heap[maxindex] = self.heap[maxindex], self.heap[i]
@@ -36,31 +47,19 @@ class Priority_Queue_mod(pq.priority_queue):
 
 def main():
     n, m = map(int, raw_input().split())
-    input = [1 for _ in range(20)]
+    input = map(int, raw_input().split())
     queue = Priority_Queue_mod([(i, 0) for i in range(n)])
-    print queue.heap
 
     for i in input:
         values = queue.extract_max()
         queue.insert((values[0], values[1]+i))
         print ' '.join(map(str, values))
-    print queue.heap
+
 
 
 if __name__ == '__main__':
     main()
 
 
-'''
-#array = [map(int, raw_input().split()) for _ in range(5)]
-array = [[1,3],[2,2],[3,5],[0,1],[4,6]]
-q = Priority_Queue_mod(array)
-q.build_heap()
-print q.heap
-q.insert([2,1])
-print q.heap
-print q.extract_max()
-print q.heap
-'''
 
 
