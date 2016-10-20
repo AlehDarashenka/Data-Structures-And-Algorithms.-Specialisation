9331a2637f0b203ff3ef7db8a3eae774e5a27a44
AlehDarashenka <aleh.darashenka@gmail.com>
2016-10-20 12:17:07 +0300

    script was updated

--- 2. Data Structures/Week 2. Priority Queues and Disjoint Sets/1. Convert array into heap.py
+++ 2. Data Structures/Week 2. Priority Queues and Disjoint Sets/1. Convert array into heap.py
@@ -1,4 +1,16 @@
 #Uses Python2
+'''
+The first step of the HeapSort algorithm is to create a heap from the array you want to sort. By the
+way, did you know that algorithms based on Heaps are widely used for external sort, when you need
+to sort huge files that don’t fit into memory of a computer?
+Your task is to implement this first step and convert a given array of integers into a heap. You will
+do that by applying a certain number of swaps to the array. Swap is an operation which exchanges
+elements 𝑎𝑖 and 𝑎𝑗 of the array 𝑎 for some 𝑖 and 𝑗. You will need to convert the array into a heap
+using only O(n) swaps, as was described in the lectures. Note that you will need to use a min-heap
+instead of a max-heap in this problem.
+
+'''
+
 class priority_queue:
 
     def __init__(self,  array):
@@ -11,7 +23,6 @@ class priority_queue:
 
     def right_child(self, i):
         self.index = i*2 + 2
-
 
     def sift_down(self, i):
         maxindex = i
@@ -30,6 +41,13 @@ class priority_queue:
             print i, maxindex
             self.sift_down(maxindex)
 
+    def extract_max(self):
+        result = self.heap[0]
+        self.heap[0] = self.heap[self.size-1]
+        self.size -= 1
+        self.sift_down(0)
+        return result
+
     def build_heap(self):
         for i in range(self.size/2, 0, -1):
             self.sift_down(i-1)
@@ -45,8 +63,7 @@ def main():
         print 0
 
 
-if __name__ == '__main__':
-    main()
+main()
 
 
 
